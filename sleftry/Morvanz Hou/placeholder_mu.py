ead54c92e457d1383669fa48a52de531a7d5c6a3
joycejhang <42739172+joycejhang@users.noreply.github.com>
2019-02-13 17:37:47 +0800

    Add files via upload

---
+++ sleftry/Morvanz Hou/placeholder_mu.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 27 12:31:03 2018
+
+@author: Joyce
+"""
+
+import tensorflow as tf
+
+#在 Tensorflow 中需要定义 placeholder 的 type ，一般为 float32 形式
+input1 = tf.placeholder(tf.float32)
+input2 = tf.placeholder(tf.float32)
+
+# mul = multiply 是将input1和input2 做乘法运算，并输出为 output 
+ouput = tf.multiply(input1, input2)
+
+with tf.Session() as sess:
+    print(sess.run(ouput, feed_dict={input1: [7.], input2: [2.]}))
+# [ 14.]
---
+++ sleftry/Morvanz Hou/variable_mu.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 27 12:24:30 2018
+
+@author: Joyce
+"""
+
+import tensorflow as tf
+
+state = tf.Variable(0, name='counter')
+
+# 定义常量 one
+one = tf.constant(1)
+
+# 定义加法步骤 (注: 此步并没有直接计算)
+new_value = tf.add(state, one)
+
+# 将 State 更新成 new_value
+update = tf.assign(state, new_value)
+
+# 如果定义 Variable, 就一定要 initialize
+# init = tf.initialize_all_variables() # tf 马上就要废弃这种写法
+init = tf.global_variables_initializer()  # 替换成这样就好
+ 
+# 使用 Session
+with tf.Session() as sess:
+    sess.run(init)
+    for _ in range(3):
+        sess.run(update)
+        print(sess.run(state))
